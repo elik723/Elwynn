1ed031ed2a90f7e223f2c0380558ba99dfdb1eac
Eli Kalish <elik723@seas.upenn.edu>
2019-07-02 12:02:04 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController,
+              public toastController: ToastController) {}
+
+  public myDateOfBirth: String;
+
+  ionViewDidEnter() {
+    this.myDateOfBirth = "1994-10-03";
+  }
+
+  pressMe() {
+    alert("Hi!!");
+  }
+
+  async funAlert() {
+    const alert = await this.alertController.create({
+      header: 'Fun Alert',
+      subHeader: 'fun alert (but smaller)',
+      message: 'Bang bang you just clicked a fun alert',
+      buttons: [{
+        text: 'Wow thats sick dawg',
+        handler: () => {
+          this.openToast();
+        }
+        }]
+    });
+
+    await alert.present();
+  }
+
+  async openToast() {
+    const toast = await this.toastController.create({
+      message: 'You just got toasted, within an alert. Thats showbiz baby',
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
